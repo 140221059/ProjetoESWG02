a42a626e90953ffff88147ab033a0bca86ab5f88
Miguel Parro <140221059@estudantes.ips.pt>
2017-12-17 03:56:01 +0000

    bd adicionada com sucesso

---
+++ projetoesw3rd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projetoesw3rd.MvcApplication" Language="C#" %>
---
+++ projetoesw3rd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(projetoesw3rd.Startup))]
+namespace projetoesw3rd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
